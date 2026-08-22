ea473bee65d68f51b6a458a7dab4831c8d18cefb
Ari Sulistiono <ari.sulistiono@gmail.com>
2026-08-22 14:43:08 +0700

    chore: localize settings toolbar control

--- src/components/ui/BottomToolbar/SettingsButton.svelte
+++ src/components/ui/BottomToolbar/SettingsButton.svelte
@@ -4,8 +4,8 @@
 	import { __settingsDrawerHidden } from '$utils/stores';
 </script>
 
-<button type="button" title="Settings" on:click={() => ($__settingsDrawerHidden = false)} class="inline-flex flex-col items-center justify-center px-5 group hover:bg-theme-accent/5" data-umami-event="Toolbar Settings Button">
+<button type="button" title="Pengaturan" on:click={() => ($__settingsDrawerHidden = false)} class="inline-flex flex-col items-center justify-center px-5 group hover:bg-theme-accent/5" data-umami-event="Toolbar Settings Button">
 	<SettingsFilled size={5} />
-	<span class="sr-only">Settings</span>
+	<span class="sr-only">Pengaturan</span>
 </button>
-<Tooltip arrow={false} type="light" class="hidden md:block font-normal">Settings</Tooltip>
+<Tooltip arrow={false} type="light" class="hidden md:block font-normal">Pengaturan</Tooltip>
