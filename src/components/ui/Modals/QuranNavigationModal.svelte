2a8150bf7c174931e71923a275a1a200e39afac3
Ari Sulistiono <ari.sulistiono@gmail.com>
2026-08-22 15:18:38 +0700

    chore: localize remaining navigation caption

--- src/components/ui/Modals/QuranNavigationModal.svelte
+++ src/components/ui/Modals/QuranNavigationModal.svelte
@@ -18,7 +18,6 @@
 	import { fetchAndCacheJson } from '$utils/fetchData';
 	import { getWebsiteWidth } from '$utils/getWebsiteWidth';
 
-	// CSS classes
 	const linkClasses = 'flex flex-row space-x-2 items-center';
 	const linkTextClasses = 'px-4 py-2 rounded-3xl border border-theme-accent/20 hover:bg-theme-accent/5 w-fit text-sm';
 	const listItemClasses = 'py-2 px-2 text-sm w-full text-left font-normal rounded-3xl border border-transparent hover:border-theme-accent hover:bg-theme-accent/5';
@@ -33,50 +32,41 @@
 	let searchResults;
 	let morphologyKey = '1:1';
 
-	// Hide the navigation modal when page URL or chapter/page number changes
 	$: if ($page.url.href || $__pageURL || $__chapterNumber || $__pageNumber) {
 		__quranNavigationModalVisible.set(false);
 	}
 
-	// Clear the searched key when the current page changes
 	$: if ($__currentPage) {
 		searchedKey = '';
 	}
 
-	// Get the number of verses in the current chapter
 	$: chapterVerses = quranMetaData[$__chapterNumber]?.verses ?? 0;
 
-	// Validate the searched key and fetch search results
 	$: (async () => {
 		searchResults = await validateKey(searchedKey);
 	})();
 
-	// Update the max number of verses to load
 	$: {
 		maxVersesLoaded = false;
 		maxVersesToLoad = Math.min(chapterVerses, maxItemsToLoad);
 	}
 
-	// Load verse key data externally to reduce bundle size
 	$: if ($__quranNavigationModalVisible || ['mushaf', 'morphology'].includes($__currentPage)) {
 		(async () => {
 			verseKeyData = await fetchAndCacheJson(cdnStaticDataUrls.verseKeyData, 'other');
 		})();
 	}
 
-	// Focus the search input box
 	$: if ($__quranNavigationModalVisible) {
 		setTimeout(() => {
 			document.getElementById('searchKey').focus();
 		}, 1);
 	}
 
-	// Update morphology key for the morphology page
 	$: if ($__morphologyKey) {
 		morphologyKey = $__morphologyKey.split(':').slice(0, 2).join(':');
 	}
 
-	// Load all chapters when necessary
 	function loadMaxChapters() {
 		if (!maxChaptersLoaded) {
 			maxItemsToLoad = 114;
@@ -84,7 +74,6 @@
 		}
 	}
 
-	// Load all verses when necessary
 	function loadMaxVerses() {
 		if (!maxVersesLoaded) {
 			maxVersesToLoad = chapterVerses;
@@ -95,7 +84,6 @@
 
 <Modal id="quranNavigationModal" bind:open={$__quranNavigationModalVisible} transitionParams={getModalTransition('top')} title="Navigasi" size="md" class="!rounded-t-none md:!rounded-3xl" bodyClass="md:p-2 !border-t-0" headerClass="hidden" placement="center" position="top" outsideclose>
 	<div class={`${getWebsiteWidth($__wideWesbiteLayoutEnabled)} flex flex-col space-y-2 justify-between px-4 py-5 mx-auto`}>
-		<!-- search block -->
 		<div id="search-block" class="mx-2">
 			<div id="navigation-inputs" class="flex flex-col justify-start">
 				<div class="flex flex-row w-full h-fit items-center">
@@ -107,15 +95,12 @@
 					</form>
 				</div>
 
-				<!-- instructions -->
 				<div id="search-instructions" class="text-xs pt-2 opacity-70 pb-4">
 					Masukkan nama {term('chapter').toLowerCase()}, nomor halaman, {term('juz').toLowerCase()}/{term('hizb').toLowerCase()}, atau kunci {term('verse').toLowerCase()}/kata seperti 2:255, 2.286, 18-10, atau 2 1 1.
 				</div>
 
-				<!-- suggestions (only for home page) -->
 				{#if searchedKey.length === 0 && $__currentPage === 'home'}
 					<div id="search-suggestions" class="flex flex-col text-base md:text-lg max-h-64 min-h-64 overflow-y-auto">
-						<!-- Last Read -->
 						{#if Object.prototype.hasOwnProperty.call($__lastRead, 'chapter')}
 							{@const lastReadChapter = $__lastRead.chapter}
 							{@const lastReadVerse = $__lastRead.verse}
@@ -128,7 +113,6 @@
 							</div>
 						{/if}
 
-						<!-- Suggestions -->
 						<div id="suggestions-links" class="py-2 space-y-2">
 							<span class="text-xs font-semibold pt-2">Saran</span>
 							{#each Object.entries(mostRead) as [_, item]}
@@ -141,13 +125,10 @@
 					</div>
 				{/if}
 
-				<!-- results -->
 				{#if searchedKey.length > 0}
 					<div id="search-results" class="flex flex-col text-base md:text-lg max-h-64 min-h-64 overflow-y-auto">
 						{#if searchResults}
-							<!-- stuff for current chapter -->
 							{#each Object.entries(searchResults) as [key, value]}
-								<!-- numbers -->
 								{#if $__chapterNumber !== 'mushaf'}
 									{#if key === 'verse' && $__currentPage === 'chapter'}
 										<div id="current-chapter-links" class="py-2 space-y-2">
@@ -161,11 +142,9 @@
 								{/if}
 							{/each}
 
-							<!-- Navigate options/links -->
 							<div id="navigate-links" class="py-2 space-y-2">
 								<span class="text-xs font-semibold">Navigasi</span>
 								{#each Object.entries(searchResults) as [key, value]}
-									<!-- numbers -->
 									{#if $__currentPage === 'mushaf'}
 										{#if key === 'chapter'}
 											<div class={linkClasses}>
@@ -175,11 +154,10 @@
 										{:else if key === 'page'}
 											<div class={linkClasses}>
 												<span>⟶</span>
-												<a href={`${base}/page?id=${value}`} class={linkTextClasses}>Page {value}</a>
+												<a href={`${base}/page?id=${value}`} class={linkTextClasses}>Halaman {value}</a>
 											</div>
 										{/if}
 
-										<!-- only show results of key-pages if we have loaded the data -->
 										{#await verseKeyData then verseKeyData}
 											{#if key === 'juz'}
 												<div class={linkClasses}>
@@ -241,7 +219,6 @@
 										{/if}
 									{/if}
 
-									<!-- word key -->
 									{#if key === 'word'}
 										<div class={linkClasses}>
 											<span>⟶</span>
@@ -249,7 +226,6 @@
 										</div>
 									{/if}
 
-									<!-- chapter names -->
 									{#if key === 'chapters' && Object.keys(value).length > 0}
 										{#each Object.entries(value) as [key, _value]}
 											<div class={linkClasses}>
@@ -265,7 +241,6 @@
 							</div>
 						{/if}
 
-						<!-- always allow user to search the Quran -->
 						{#if searchedKey.length > 0}
 							<div id="search-quran" class="py-2 space-y-2">
 								<span class="text-xs font-semibold">Cari di Al Quran</span>
@@ -280,10 +255,8 @@
 			</div>
 		</div>
 
-		<!-- chapter and verse selectors -->
 		{#if $__currentPage !== 'home'}
 			<div id="chapter-and-verses-block" class="flex flex-row space-x-4 !mt-[-0rem] max-h-64 min-h-64 {searchedKey.length > 0 ? 'hidden' : 'block'}">
-				<!-- chapter selector -->
 				{#if !['morphology'].includes($__currentPage)}
 					<div class="flex flex-col py-2 space-y-2 w-full">
 						<div class="px-2 pb-2 text-xs font-semibold border-b border-theme-accent/20">{term('chapters')}</div>
@@ -311,7 +284,6 @@
 					</div>
 				{/if}
 
-				<!-- verse selector -->
 				{#if $__currentPage === 'chapter'}
 					<div class="flex flex-col py-2 space-y-2 w-44">
 						<div class="mx-4 pb-2 text-xs font-semibold border-b border-theme-accent/20">{term('verses')}</div>
@@ -334,23 +306,6 @@
 					</div>
 				{/if}
 
-				<!-- supplications selector -->
-				<!-- {#if $__currentPage === 'supplications'}
-					<div class="flex flex-col space-y-2 w-full">
-						<div class="px-2 text-sm pb-2 border-b border-theme-accent/20 font-medium">{term('supplications')}</div>
-						<ul id="navbar-supplications-list" class="grow basis-1/2 px-2 overflow-y-scroll">
-							{#each Object.entries(supplicationsFromQuran) as [key, value]}
-								<li>
-									<a href="#{key}">
-										<div class={listItemClasses}>{quranMetaData[key.split(':')[0]].transliteration}, {key}</div>
-									</a>
-								</li>
-							{/each}
-						</ul>
-					</div>
-				{/if} -->
-
-				<!-- words selector -->
 				{#if $__currentPage === 'morphology'}
 					<div class="flex flex-col space-y-2 w-full">
 						<div class="px-2 text-sm pb-2 border-b border-theme-accent/20 font-medium">Kata</div>
