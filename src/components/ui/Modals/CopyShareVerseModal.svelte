23e29fa4cc798cef3c991fa9504d6211cc894bac
Ari Sulistiono <ari.sulistiono@gmail.com>
2026-08-22 14:47:36 +0700

    chore: localize copy and share modal

--- src/components/ui/Modals/CopyShareVerseModal.svelte
+++ src/components/ui/Modals/CopyShareVerseModal.svelte
@@ -2,7 +2,7 @@
 	import Modal from '$ui/FlowbiteSvelte/modal/Modal.svelte';
 	import Radio from '$ui/FlowbiteSvelte/forms/Radio.svelte';
 	import Checkbox from '$ui/FlowbiteSvelte/forms/Checkbox.svelte';
-	
+
 	import { __verseKey, __copyShareVerseModalVisible, __verseTranslationData } from '$utils/stores';
 	import { linkClasses, buttonClasses, selectedRadioOrCheckboxClasses } from '$data/commonClasses';
 	import { term } from '$utils/terminologies';
@@ -10,10 +10,8 @@
 	import { selectableVerseTranslations } from '$data/options';
 	import { getChapterDisplayMeta } from '$utils/chapterLocalization';
 
-	// CSS classes for radio buttons
 	const radioClasses = 'inline-flex justify-between items-center py-2 px-4 w-full bg-theme-bg rounded-lg border-2 border-theme-accent/20 cursor-pointer peer-checked:border-2 peer-checked:border-theme-accent hover:bg-theme-accent/5';
 
-	// Options
 	let copyType = 1;
 	let textType = 1;
 	let includeKey = true;
@@ -23,28 +21,16 @@
 	let generatedVerseData = '';
 	let websiteLink = '';
 
-	// Extract chapter number from verse key
 	$: [chapter, verse] = $__verseKey.split(':').map(Number);
-
-	// Shareable link
 	$: websiteLink = `https://quranwbw.com/${chapter}/${verse}`;
-
-	// Reset the generated data variable whenever any of the option changes
-	$: if ($__copyShareVerseModalVisible || $__verseKey || copyType || textType || includeKey || includeTranslationNames || includeFootNotes || includeLink) {
-		generatedVerseData = '';
-	}
+	$: if ($__copyShareVerseModalVisible || $__verseKey || copyType || textType || includeKey || includeTranslationNames || includeFootNotes || includeLink) generatedVerseData = '';
 
 	function getVerseArabicText(key) {
 		try {
 			const [chapter, verse] = key.split(':').map(Number);
 			const words = document.querySelectorAll(`.verse-${chapter}-${verse} .arabicText`);
 			let wordsArray = [];
-
-			// Join all the words
-			words.forEach((word) => {
-				wordsArray.push(word.innerText);
-			});
-
+			words.forEach((word) => wordsArray.push(word.innerText));
 			return wordsArray.join(' ');
 		} catch (error) {
 			console.warn(error);
@@ -58,7 +44,6 @@
 		let footnoteCounter = 1;
 		const allFootnotes = [];
 
-		// Collect translations
 		for (const translationId in $__verseTranslationData) {
 			const translation = $__verseTranslationData[translationId];
 			if (translation && translation[key]) {
@@ -71,23 +56,14 @@
 			}
 		}
 
-		// Format output
 		for (const entry of allTranslations) {
-			// Replace <sup foot_note=X>Y</sup> with [N]
 			let text = entry.text.replace(/<sup[^>]*?>\d+<\/sup>/g, () => `[${footnoteCounter++}]`);
-
-			// Remove remaining HTML tags
 			text = text.replace(/<[^>]+>/g, '');
-
 			output += text;
 
-			if (includeTranslationNames) {
-				output += `\n— ${entry.authorName}`;
-			}
-
+			if (includeTranslationNames) output += `\n— ${entry.authorName}`;
 			output += `\n\n`;
 
-			// Add footnotes
 			if (includeFootnotes && entry.footnotes) {
 				for (const footnote of entry.footnotes) {
 					const cleanFootnote = footnote.replace(/<[^>]+>/g, '').trim();
@@ -104,48 +80,27 @@
 		let results = '';
 		const [chapter] = key.split(':');
 
-		// Add verse key on top
-		if (includeKey) {
-			results += `${getChapterDisplayMeta(chapter).transliteration}, ${key}\n\n`;
-		}
+		if (includeKey) results += `${getChapterDisplayMeta(chapter).transliteration}, ${key}\n\n`;
 
-		// Get Arabic and/or translation text
 		const arabicText = getVerseArabicText(key);
 		const translationText = getVerseTranslationText(key, includeTranslationNames, includeFootNotes);
 
-		if (textType === 1) {
-			// Only Arabic
-			results += `${arabicText}\n\n`;
-		} else if (textType === 2) {
-			// Only translation
-			results += `${translationText}\n\n`;
-		} else if (textType === 3) {
-			// Both Arabic and translation
-			results += `${arabicText}\n\n${translationText}\n\n`;
-		}
+		if (textType === 1) results += `${arabicText}\n\n`;
+		else if (textType === 2) results += `${translationText}\n\n`;
+		else if (textType === 3) results += `${arabicText}\n\n${translationText}\n\n`;
 
-		// Add website link at the end
-		if (includeLink) {
-			results += websiteLink;
-		}
-
+		if (includeLink) results += websiteLink;
 		return results.trim();
 	}
 
 	async function processAndCopyVerseData() {
-		if (copyType === 1) {
-			generatedVerseData = getVerseArabicText($__verseKey);
-		} else if (copyType === 2) {
-			generatedVerseData = websiteLink;
-		} else if (copyType === 3) {
-			generatedVerseData = advancedCopy($__verseKey, textType, includeKey, includeTranslationNames, includeFootNotes, includeLink);
-		}
+		if (copyType === 1) generatedVerseData = getVerseArabicText($__verseKey);
+		else if (copyType === 2) generatedVerseData = websiteLink;
+		else if (copyType === 3) generatedVerseData = advancedCopy($__verseKey, textType, includeKey, includeTranslationNames, includeFootNotes, includeLink);
 
-		console.log(generatedVerseData);
 		navigator.clipboard.writeText(generatedVerseData);
 	}
 
-	// function to download any text/string as a text file
 	function downloadTextFile(name, data) {
 		const link = document.createElement('a');
 		const file = new Blob([data], { type: 'text/plain' });
@@ -162,97 +117,79 @@
 	</h3>
 
 	<div class="flex-1 min-h-0 overflow-y-auto w-full pr-2">
-		<!-- Copy Type -->
 		<div class="flex flex-col space-y-4 py-4">
-			<span class="text-sm">Type</span>
+			<span class="text-sm">Jenis</span>
 			<div class="flex flex-row space-x-2">
 				<div class="flex items-center">
 					<Radio bind:group={copyType} value={1} custom>
-						<div class="{radioClasses} {copyType === 1 && selectedRadioOrCheckboxClasses}">
-							<div class="w-full">Text</div>
-						</div>
+						<div class="{radioClasses} {copyType === 1 && selectedRadioOrCheckboxClasses}"><div class="w-full">Teks</div></div>
 					</Radio>
 				</div>
 				<div class="flex items-center">
 					<Radio bind:group={copyType} value={2} custom>
-						<div class="{radioClasses} {copyType === 2 && selectedRadioOrCheckboxClasses}">
-							<div class="w-full">Link</div>
-						</div>
+						<div class="{radioClasses} {copyType === 2 && selectedRadioOrCheckboxClasses}"><div class="w-full">Tautan</div></div>
 					</Radio>
 				</div>
 				<div class="flex items-center">
 					<Radio bind:group={copyType} value={3} custom>
-						<div class="{radioClasses} {copyType === 3 && selectedRadioOrCheckboxClasses}">
-							<div class="w-full">Advanced</div>
-						</div>
+						<div class="{radioClasses} {copyType === 3 && selectedRadioOrCheckboxClasses}"><div class="w-full">Lanjutan</div></div>
 					</Radio>
 				</div>
 			</div>
 
-			<!-- Type Description -->
 			{#if copyType !== 3}
 				<span class="flex flex-col space-y-3 text-xs opacity-70">
 					{#if copyType === 1}
-						<span>Copy the Arabic text of the {term('verse')} only.</span>
+						<span>Salin hanya teks Arab dari {term('verse').toLowerCase()} ini.</span>
 					{:else if copyType === 2}
-						<span>Copy the website link of the {term('verse')} only.</span>
+						<span>Salin hanya tautan halaman {term('verse').toLowerCase()} ini.</span>
 					{/if}
 				</span>
 			{/if}
 		</div>
 
-		<!-- If Advanced -->
 		{#if copyType === 3}
 			<div class="flex flex-col">
-				<!-- Main Options -->
 				<div class="flex flex-col">
-					<!-- Arabic/Translation/Both -->
 					<div class="flex flex-col space-y-4 py-4 border-t border-theme-accent/20">
-						<span class="text-sm">Text</span>
+						<span class="text-sm">Teks</span>
 						<div class="flex flex-row space-x-2">
 							<div class="flex items-center">
 								<Radio bind:group={textType} value={1} custom>
-									<div class="{radioClasses} {textType === 1 && selectedRadioOrCheckboxClasses}">
-										<div class="w-full">Arabic</div>
-									</div>
+									<div class="{radioClasses} {textType === 1 && selectedRadioOrCheckboxClasses}"><div class="w-full">Arab</div></div>
 								</Radio>
 							</div>
 							<div class="flex items-center">
 								<Radio bind:group={textType} value={2} custom>
-									<div class="{radioClasses} {textType === 2 && selectedRadioOrCheckboxClasses}">
-										<div class="w-full">Translation</div>
-									</div>
+									<div class="{radioClasses} {textType === 2 && selectedRadioOrCheckboxClasses}"><div class="w-full">Terjemahan</div></div>
 								</Radio>
 							</div>
 							<div class="flex items-center">
 								<Radio bind:group={textType} value={3} custom>
-									<div class="{radioClasses} {textType === 3 && selectedRadioOrCheckboxClasses}">
-										<div class="w-full">Both</div>
-									</div>
+									<div class="{radioClasses} {textType === 3 && selectedRadioOrCheckboxClasses}"><div class="w-full">Keduanya</div></div>
 								</Radio>
 							</div>
 						</div>
 					</div>
 				</div>
 
-				<!-- Other Options -->
 				<div class="flex flex-col space-y-2 py-4 border-t border-theme-accent/20">
 					<Checkbox checked={includeKey} on:click={() => (includeKey = !includeKey)} class="space-x-2 pb-2 font-normal bg-theme-bg">
-						<span>Include {term('chapter')} Name & {term('verse')} Key</span>
+						<span>Sertakan Nama {term('chapter')} & Nomor {term('verse')}</span>
 					</Checkbox>
 
 					{#if textType === 2 || textType === 3}
 						<Checkbox checked={includeTranslationNames} on:click={() => (includeTranslationNames = !includeTranslationNames)} class="space-x-2 pb-2 font-normal bg-theme-bg">
-							<span>Include Author Names</span>
+							<span>Sertakan Nama Penerjemah</span>
 						</Checkbox>
 
 						<Checkbox checked={includeFootNotes} on:click={() => (includeFootNotes = !includeFootNotes)} class="space-x-2 pb-2 font-normal bg-theme-bg">
-							<span>Include Footnotes</span>
+							<span>Sertakan Catatan Kaki</span>
 						</Checkbox>
 					{/if}
 
 					<Checkbox checked={includeLink} on:click={() => (includeLink = !includeLink)} class="space-x-2 pb-2 font-normal bg-theme-bg">
-						<span>Include Website Link</span>
+						<span>Sertakan Tautan Situs</span>
 					</Checkbox>
 				</div>
 			</div>
@@ -261,16 +198,16 @@
 		{#if generatedVerseData !== ''}
 			<div class="text-xs opacity-70 mb-6 text-left">
 				{#if copyType === 1 || copyType === 3}
-					<span>Text copied to clipboard.</span>
-					<button on:click={downloadTextFile(`quran-${chapter}-${verse}`, generatedVerseData)} class={linkClasses} data-umami-event="Download Verse File Button">Click here to download it as a file.</button>
+					<span>Teks telah disalin ke papan klip.</span>
+					<button on:click={downloadTextFile(`quran-${chapter}-${verse}`, generatedVerseData)} class={linkClasses} data-umami-event="Download Verse File Button">Unduh sebagai berkas teks.</button>
 				{:else}
-					<span>Link copied to clipboard.</span>
+					<span>Tautan telah disalin ke papan klip.</span>
 				{/if}
 			</div>
 		{/if}
 	</div>
 
 	<div class="flex-shrink-0 mt-4">
-		<button class="w-full {buttonClasses}" on:click={processAndCopyVerseData} data-umami-event="Copy Verse Button">Copy</button>
+		<button class="w-full {buttonClasses}" on:click={processAndCopyVerseData} data-umami-event="Copy Verse Button">Salin</button>
 	</div>
 </Modal>
