0cb8d0bfaa17a8d564adce437226781c2a300726
Ari Sulistiono <ari.sulistiono@gmail.com>
2026-08-22 14:45:05 +0700

    chore: localize bookmarks page

--- src/routes/bookmarks/+page.svelte
+++ src/routes/bookmarks/+page.svelte
@@ -5,18 +5,17 @@
 	import { __currentPage, __displayType, __userBookmarks } from '$utils/stores';
 	import { term } from '$utils/terminologies';
 
-	// Allow only supported display types; fallback to default without saving to settings
 	if ([3, 4].includes($__displayType)) $__displayType = 1;
 
 	__currentPage.set('bookmarks');
 </script>
 
-<PageHead title={'Bookmarks'} />
+<PageHead title={'Penanda Ayat'} />
 
 <div id="individual-verses-block">
 	{#if $__userBookmarks.length === 0}
 		<div class="flex flex-row justify-center text-xs md:text-sm opacity-70">
-			<span class="leading-relaxed">You haven't bookmarked any {term('verse')} yet! Start by clicking on the <Bookmark classes="inline mt-[-4px] mx-1" /> icon for a {term('verse')}. It's a perfect way to return to the {term('verses')} that resonate with you. </span>
+			<span class="leading-relaxed">Belum ada {term('verse').toLowerCase()} yang ditandai. Ketuk ikon <Bookmark classes="inline mt-[-4px] mx-1" /> pada {term('verse').toLowerCase()} yang ingin disimpan agar mudah dibuka kembali nanti.</span>
 		</div>
 	{:else}
 		<FullVersesDisplay keys={$__userBookmarks.toString()} />
