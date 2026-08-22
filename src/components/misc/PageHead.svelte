f6ae22c266a0794c5b5f3f992a08b3867914d832
Ari Sulistiono <ari.sulistiono@gmail.com>
2026-08-22 15:11:31 +0700

    chore: localize page titles and metadata

--- src/components/misc/PageHead.svelte
+++ src/components/misc/PageHead.svelte
@@ -1,10 +1,29 @@
 <script>
 	export let title;
 
-	import { websiteTagline, websiteURL, wbwLanguages } from '$data/websiteSettings';
+	import { websiteTagline, websiteURL } from '$data/websiteSettings';
+
+	const exactTitles = {
+		Bookmarks: 'Penanda Ayat',
+		Topics: 'Topik',
+		Search: 'Cari',
+		Settings: 'Pengaturan',
+		About: 'Tentang',
+		Changelog: 'Pembaruan',
+		Morphology: 'Morfologi',
+		'Offline Mode (Beta)': 'Mode Offline (Beta)'
+	};
+
+	function localizeTitle(value = '') {
+		if (exactTitles[value]) return exactTitles[value];
+		if (/^Page\s+\d+/i.test(value)) return value.replace(/^Page/i, 'Halaman');
+		return value;
+	}
+
+	$: localizedTitle = localizeTitle(title);
 </script>
 
 <svelte:head>
-	<title>{title} - {websiteURL}</title>
-	<meta name="description" content="{title} - {websiteTagline} in {wbwLanguages} - {websiteURL}" />
+	<title>{localizedTitle} - {websiteURL}</title>
+	<meta name="description" content="{localizedTitle} - {websiteTagline}. Aplikasi Al Quran berbahasa Indonesia." />
 </svelte:head>
