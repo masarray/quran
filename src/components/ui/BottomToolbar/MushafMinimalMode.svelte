753db74408b16459e1d0d942cd75f080ec949ae0
Ari Sulistiono <ari.sulistiono@gmail.com>
2026-08-22 14:43:51 +0700

    chore: localize minimal mushaf mode label

--- src/components/ui/BottomToolbar/MushafMinimalMode.svelte
+++ src/components/ui/BottomToolbar/MushafMinimalMode.svelte
@@ -4,8 +4,8 @@
 	import { toggleMushafMinimalMode } from '$utils/toggleMushafMinimalMode';
 </script>
 
-<button id="mushafMinimalModeButton" type="button" title="Minimal Mode" class="inline-flex flex-col items-center justify-center px-5 group hover:bg-theme-accent/5" on:click={toggleMushafMinimalMode}>
+<button id="mushafMinimalModeButton" type="button" title="Mode Minimal" class="inline-flex flex-col items-center justify-center px-5 group hover:bg-theme-accent/5" on:click={toggleMushafMinimalMode}>
 	<Maximize />
-	<span class="sr-only">Minimal Mode</span>
+	<span class="sr-only">Mode Minimal</span>
 </button>
-<Tooltip triggeredBy="#mushafMinimalModeButton" arrow={false} type="light" class="hidden md:block font-normal">Minimal Mode</Tooltip>
+<Tooltip triggeredBy="#mushafMinimalModeButton" arrow={false} type="light" class="hidden md:block font-normal">Mode Minimal</Tooltip>
