19b2a8db385ff300e04d58531f9b9c590794d42e
Ari Sulistiono <ari.sulistiono@gmail.com>
2026-08-22 15:12:00 +0700

    chore: localize supplications page title

--- src/views/Supplications.svelte
+++ src/views/Supplications.svelte
@@ -3,13 +3,12 @@
 	import PageHead from '$misc/PageHead.svelte';
 	import FullVersesDisplay from '$display/verses/modes/FullVersesDisplay.svelte';
 	import { __currentPage } from '$utils/stores';
-	import { term } from '$utils/terminologies';
 	import { supplicationsFromQuran } from '$data/quranMeta';
 
 	__currentPage.set('supplications');
 </script>
 
-<PageHead title={`Quranic ${term('supplications')}`} />
+<PageHead title="Doa dalam Al Quran" />
 
 <div id="individual-verses-block">
 	<FullVersesDisplay keys={Object.keys(supplicationsFromQuran).toString()} />
