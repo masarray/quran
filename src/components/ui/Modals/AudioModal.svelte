4c86c837042cbbcc716835d43fee82328bde4a4f
Ari Sulistiono <ari.sulistiono@gmail.com>
2026-08-22 14:49:36 +0700

    chore: localize advanced audio modal to Indonesian

--- src/components/ui/Modals/AudioModal.svelte
+++ src/components/ui/Modals/AudioModal.svelte
@@ -17,7 +17,6 @@
 	import { updateSettings } from '$utils/updateSettings';
 	import { defaultSettings } from '$src/hooks.client';
 
-	// CSS classes for radio buttons
 	const radioClasses = 'inline-flex justify-between items-center py-2 px-4 w-full bg-theme-bg rounded-lg border-2 border-theme-accent/20 cursor-pointer peer-checked:border-2 peer-checked:border-theme-accent hover:bg-theme-accent/5';
 	const dropdownItemClasses = 'flex flex-row items-center space-x-2 font-normal rounded-3xl hover:bg-theme-accent/5';
 	let invalidStartVerse = false;
@@ -27,84 +26,37 @@
 	let endVerseDropdownOpen = false;
 	let timesToRepeatDropdownOpen = false;
 	let audioDelayDropdownOpen = false;
-	let startVerseSearch = ''; // Holds search input for start verse
-	let endVerseSearch = ''; // Holds search input for end verse
+	let startVerseSearch = '';
+	let endVerseSearch = '';
 	$: versesInChapter = quranMetaData[$__chapterNumber].verses;
 
-	// Update settings and validate verses when audio modal is visible
 	$: if ($__audioModalVisible) {
-		window.versesToPlayArray = []; // clear verses just in case
-
+		window.versesToPlayArray = [];
 		const { startVerse, endVerse, timesToRepeat } = $__audioSettings;
-
-		// Set verses to play based on audio range setting
 		prepareVersesToPlay($__audioSettings.playingKey);
 
-		// Initialize endVerse as startVerse if undefined
-		if ($__audioSettings.endVerse == null) {
-			$__audioSettings.endVerse = startVerse;
-		}
+		if ($__audioSettings.endVerse == null) $__audioSettings.endVerse = startVerse;
 
-		// Validate verse and repeat times
 		invalidStartVerse = startVerse < 1 || startVerse > versesInChapter;
 		invalidEndVerse = endVerse < 1 || endVerse > versesInChapter || endVerse < startVerse;
 		invalidTimesToRepeat = !selectableRepeatTimes.includes(timesToRepeat);
 	}
 
-	// Allow only "playThisVerse" option for non-chapter pages
 	$: if ($__currentPage !== 'chapter' && $__audioSettings.audioRange === 'playRange') {
 		$__audioSettings.audioRange = 'playThisVerse';
 	}
 
-	// If the audio settings had to be remembered, get them from localStorage whenever the page is loaded
-	if ($__audioSettings.rememberSettings) {
-		savedPlaySettingsHandler('get');
-	}
+	if ($__audioSettings.rememberSettings) savedPlaySettingsHandler('get');
+	if ($__audioSettings.repeatType === undefined) $__audioSettings.repeatType = 'repeatVerse';
+	if ($__audioSettings.audioDelay === undefined) $__audioSettings.audioDelay = 1;
+	if ($__audioSettings.timesToRepeat === undefined || !selectableRepeatTimes.includes($__audioSettings.timesToRepeat)) $__audioSettings.timesToRepeat = 1;
 
-	// Default to verse repeat
-	if ($__audioSettings.repeatType === undefined) {
-		$__audioSettings.repeatType = 'repeatVerse';
-	}
+	$: if ($__currentPage !== 'chapter') $__audioSettings.repeatType = 'repeatVerse';
+	$: if ($__audioSettings && $__audioSettings.rememberSettings === true) savedPlaySettingsHandler('set');
+	$: if ($__chapterNumber && $__audioSettings.endVerse > versesInChapter) $__audioSettings.endVerse = versesInChapter;
+	$: if ($__audioModalVisible) $__audioSettings.endVerse = versesInChapter;
+	$: if ($__audioSettings.endVerse < $__audioSettings.startVerse) $__audioSettings.endVerse = $__audioSettings.startVerse;
 
-	// Default to no delay
-	if ($__audioSettings.audioDelay === undefined) {
-		$__audioSettings.audioDelay = 1;
-	}
-
-	// Default to repeat 1 time
-	if ($__audioSettings.timesToRepeat === undefined || !selectableRepeatTimes.includes($__audioSettings.timesToRepeat)) {
-		$__audioSettings.timesToRepeat = 1;
-	}
-
-	// For any page other than chapter page, default to verse repeat
-	$: if ($__currentPage !== 'chapter') {
-		$__audioSettings.repeatType = 'repeatVerse';
-	}
-
-	// If the audio settings had to be remembered, set them in localStorage whenever the a change is made
-	$: if ($__audioSettings && $__audioSettings.rememberSettings === true) {
-		savedPlaySettingsHandler('set');
-	}
-
-	// Properly set the max verses allowed
-	$: if ($__chapterNumber && $__audioSettings.endVerse > versesInChapter) {
-		$__audioSettings.endVerse = versesInChapter;
-	}
-
-	// Update the end verse whenever the audio modal opens
-	$: if ($__audioModalVisible) {
-		$__audioSettings.endVerse = versesInChapter;
-	}
-
-	// End verse checks
-	$: if ($__audioSettings.endVerse < $__audioSettings.startVerse) {
-		$__audioSettings.endVerse = $__audioSettings.startVerse;
-	}
-
-	// $: console.log($__audioSettings);
-
-	// This function manages the saving, retrieving, and resetting of audio settings in the $__audioSettings object.
-	// It takes an action parameter that determines whether to get ('get'), set ('set'), or reset to default ('default') the audio settings.
 	function savedPlaySettingsHandler(action) {
 		const audioSettings = $__audioSettings;
 		const savedSettings = $__audioSettings.savedPlaySettings || {};
@@ -120,9 +72,7 @@
 		};
 
 		if (action === 'get') {
-			if (Object.keys(savedSettings).length === 0) {
-				savedPlaySettingsHandler('set');
-			}
+			if (Object.keys(savedSettings).length === 0) savedPlaySettingsHandler('set');
 			assignSettings(savedSettings, audioSettings);
 		} else if (action === 'set') {
 			assignSettings(audioSettings, savedSettings);
@@ -131,15 +81,11 @@
 			delete audioSettings.savedPlaySettings;
 		}
 
-		// Setting the reciters manually because the "remember settings" option resets these for some reason
 		$__audioSettings.reciter = $__reciter;
 		$__audioSettings.translationReciter = $__translationReciter;
-
 		updateSettings({ type: 'audioSettings', value: audioSettings });
 	}
 
-	// This function toggles the rememberSettings property within the $__audioSettings object.
-	// Depending on the new state, it calls savedPlaySettingsHandler to either save or reset the audio settings.
 	function toggleRememberSettings() {
 		const rememberSettings = $__audioSettings.rememberSettings;
 		const newSetting = !rememberSettings;
@@ -155,11 +101,9 @@
 
 	<div class="flex-1 min-h-0 overflow-y-auto w-full pr-2">
 		<div class="flex flex-col">
-			<!-- play type options -->
 			<div class="flex flex-col space-y-4 py-4">
-				<span class="text-sm">Play</span>
+				<span class="text-sm">Putar</span>
 				<div class="flex flex-row space-x-2">
-					<!-- play verse -->
 					<div class="flex items-center">
 						<Radio bind:group={$__audioSettings.audioType} value="verse" custom>
 							<div class="{radioClasses} {$__audioSettings.audioType === 'verse' && selectedRadioOrCheckboxClasses}">
@@ -167,11 +111,10 @@
 							</div>
 						</Radio>
 					</div>
-					<!-- play word -->
 					<div class="flex items-center">
 						<Radio bind:group={$__audioSettings.audioType} value="word" custom>
 							<div class="{radioClasses} {$__audioSettings.audioType === 'word' && selectedRadioOrCheckboxClasses}">
-								<div class="w-full">Words</div>
+								<div class="w-full">Kata</div>
 							</div>
 						</Radio>
 					</div>
@@ -179,91 +122,80 @@
 
 				{#if $__audioSettings.audioType === 'word'}
 					<span class="flex flex-col space-y-3 text-xs pt-2 opacity-70">
-						<span>This feature allows you to hear each word in the {term('verse')} individually. To listen to specific words, simply click on them. Please note, this option plays the words sequentially without accounting for the connecting silent letters between them. For a seamless and accurate recitation, it is recommended to play the entire {term('verse')}.</span>
+						<span>Fitur ini memungkinkan Anda mendengarkan setiap kata dalam {term('verse').toLowerCase()} satu per satu. Untuk mendengarkan kata tertentu, cukup ketuk kata tersebut. Perlu diketahui, mode ini memutar kata secara berurutan tanpa memperhitungkan huruf sambung yang tidak dibaca. Untuk bacaan yang utuh dan akurat, sebaiknya putar seluruh {term('verse').toLowerCase()}.</span>
 					</span>
 				{/if}
 			</div>
 
-			<!-- language options -->
 			<div id="recitation-language-block" class="flex flex-col space-y-4 py-4 border-t border-theme-accent/20 {$__audioSettings.audioType === 'word' ? 'hidden' : null}">
-				<span class="text-sm">Language</span>
+				<span class="text-sm">Bahasa</span>
 				<div class="flex flex-row space-x-2">
-					<!-- play arabic only -->
 					<div class="flex items-center">
 						<Radio bind:group={$__audioSettings.language} value="arabic" custom>
 							<div class="{radioClasses} {$__audioSettings.language === 'arabic' && selectedRadioOrCheckboxClasses}">
-								<div class="w-full">Arabic</div>
+								<div class="w-full">Arab</div>
 							</div>
 						</Radio>
 					</div>
-					<!-- play translation only -->
 					<div class="flex items-center">
 						<Radio bind:group={$__audioSettings.language} value="translation" custom>
 							<div class="{radioClasses} {$__audioSettings.language === 'translation' && selectedRadioOrCheckboxClasses}">
-								<div class="w-full">Translation</div>
+								<div class="w-full">Terjemahan</div>
 							</div>
 						</Radio>
 					</div>
-					<!-- play both -->
 					<div class="flex items-center">
 						<Radio bind:group={$__audioSettings.language} value="both" custom>
 							<div class="{radioClasses} {$__audioSettings.language === 'both' && selectedRadioOrCheckboxClasses}">
-								<div class="w-full">Both</div>
+								<div class="w-full">Keduanya</div>
 							</div>
 						</Radio>
 					</div>
 				</div>
 			</div>
 
-			<!-- range options -->
 			<div id="single-or-range-block" class="flex flex-col space-y-4 py-4 border-t border-theme-accent/20 {$__audioSettings.audioType === 'word' ? 'hidden' : null}">
-				<span class="text-sm">Range</span>
+				<span class="text-sm">Rentang</span>
 				<div class="flex flex-row space-x-2">
-					<!-- play this verse -->
 					<div class="flex items-center min-w-fit {!['chapter', 'mushaf', 'supplications', 'bookmarks', 'juz', 'hizb'].includes($__currentPage) && disabledClasses}">
 						<Radio bind:group={$__audioSettings.audioRange} value="playThisVerse" custom>
 							<div class="{radioClasses} {$__audioSettings.audioRange === 'playThisVerse' && selectedRadioOrCheckboxClasses}">
-								<div class="w-full">This {term('verse')}</div>
+								<div class="w-full">{term('verse')} Ini</div>
 							</div>
 						</Radio>
 					</div>
-					<!-- play from here -->
 					<div class="flex items-center min-w-fit {!['chapter', 'mushaf', 'supplications', 'bookmarks', 'juz', 'hizb'].includes($__currentPage) && disabledClasses}">
 						<Radio bind:group={$__audioSettings.audioRange} value="playFromHere" custom>
 							<div class="{radioClasses} {$__audioSettings.audioRange === 'playFromHere' && selectedRadioOrCheckboxClasses}">
-								<div class="w-full">From Here</div>
+								<div class="w-full">Dari Sini</div>
 							</div>
 						</Radio>
 					</div>
-					<!-- play range -->
 					<div class="flex items-center min-w-fit {!['chapter'].includes($__currentPage) && disabledClasses}">
 						<Radio bind:group={$__audioSettings.audioRange} value="playRange" custom>
 							<div class="{radioClasses} {$__audioSettings.audioRange === 'playRange' && selectedRadioOrCheckboxClasses}">
-								<div class="w-full">Custom</div>
+								<div class="w-full">Kustom</div>
 							</div>
 						</Radio>
 					</div>
 				</div>
 			</div>
 
-			<!-- repeat type options -->
 			{#if $__audioSettings.audioRange === 'playRange'}
 				<div id="repeat-type-block" class="flex flex-col space-y-4 py-4 border-t border-theme-accent/20 {$__audioSettings.audioType === 'word' ? 'hidden' : null}">
-					<span class="text-sm">Repeat</span>
+					<span class="text-sm">Pengulangan</span>
 					<div class="flex flex-row space-x-2">
-						<!-- repeat each verse -->
 						<div class="flex items-center min-w-fit {!['chapter', 'mushaf', 'supplications', 'bookmarks', 'juz', 'hizb'].includes($__currentPage) && disabledClasses}">
 							<Radio bind:group={$__audioSettings.repeatType} value="repeatVerse" custom>
 								<div class="{radioClasses} {$__audioSettings.repeatType === 'repeatVerse' && selectedRadioOrCheckboxClasses}">
-									<div class="w-full">Each {term('verse')}</div>
+									<div class="w-full">Setiap {term('verse')}</div>
 								</div>
 							</Radio>
 						</div>
-						<!-- repeat whole range -->
 						<div class="flex items-center min-w-fit {!['chapter', 'mushaf', 'supplications', 'bookmarks', 'juz', 'hizb'].includes($__currentPage) && disabledClasses}">
 							<Radio bind:group={$__audioSettings.repeatType} value="repeatRange" custom>
 								<div class="{radioClasses} {$__audioSettings.repeatType === 'repeatRange' && selectedRadioOrCheckboxClasses}">
-									<div class="w-full">{term('verse')} Range</div>
+									<div class="w-full">Seluruh Rentang</div>
 								</div>
 							</Radio>
 						</div>
@@ -272,27 +204,21 @@
 			{/if}
 		</div>
 
-		<!-- from/till verse options -->
 		{#if $__currentPage === 'chapter' && $__audioSettings.audioType === 'verse'}
 			<div id="audio-range-options" class={$__audioSettings.audioRange === 'playRange' ? 'block' : 'hidden'}>
 				<div class="flex flex-col space-y-4 py-4 border-t border-theme-accent/20">
 					<div class="flex flex-row space-x-4">
-						<!-- Start Verse Dropdown -->
 						<div class="flex flex-row space-x-2">
-							<span class="m-auto text-sm">From</span>
+							<span class="m-auto text-sm">Dari</span>
 
-							<button class="{buttonClasses} text-sm">
-								<div>{term('verse')} {$__audioSettings.startVerse}</div>
-							</button>
+							<button class="{buttonClasses} text-sm"><div>{term('verse')} {$__audioSettings.startVerse}</div></button>
 							<Dropdown bind:open={startVerseDropdownOpen} class="w-max">
-								<!-- Sticky Search Box -->
 								<div class="p-2 sticky top-0 z-10">
-									<Input min="1" max={versesInChapter} type="number" bind:value={startVerseSearch} autocomplete="off" placeholder="{term('verse')} #" size="md" class="bg-transparent rounded-3xl px-4 w-32 placeholder:text-theme-accent/50">
+									<Input min="1" max={versesInChapter} type="number" bind:value={startVerseSearch} autocomplete="off" placeholder="Nomor {term('verse')}" size="md" class="bg-transparent rounded-3xl px-4 w-32 placeholder:text-theme-accent/50">
 										<Search slot="left" size={6} classes="pt-1 {startVerseSearch.length > 0 && 'hidden'}" />
 									</Input>
 								</div>
 
-								<!-- Scrollable List -->
 								<div class="max-h-52 overflow-y-auto my-2 px-2">
 									{#each Array.from({ length: versesInChapter }, (_, i) => i + 1).filter((v) => v.toString().includes(startVerseSearch)) as verse}
 										<DropdownItem
@@ -302,30 +228,24 @@
 												startVerseDropdownOpen = !startVerseDropdownOpen;
 											}}
 										>
-											{term('verse')}
-											{verse}
+											{term('verse')} {verse}
 										</DropdownItem>
 									{/each}
 								</div>
 							</Dropdown>
 						</div>
 
-						<!-- End Verse Dropdown -->
 						<div class="flex flex-row space-x-2">
-							<span class="m-auto text-sm">Till</span>
+							<span class="m-auto text-sm">Sampai</span>
 
-							<button class="{buttonClasses} text-sm">
-								<div>{term('verse')} {$__audioSettings.endVerse}</div>
-							</button>
+							<button class="{buttonClasses} text-sm"><div>{term('verse')} {$__audioSettings.endVerse}</div></button>
 							<Dropdown bind:open={endVerseDropdownOpen} class="w-max">
-								<!-- Sticky Search Box -->
 								<div class="p-2 sticky top-0 z-10">
-									<Input min="1" max={versesInChapter} type="number" bind:value={endVerseSearch} autocomplete="off" placeholder="{term('verse')} #" size="md" class="bg-transparent rounded-3xl px-4 w-32 placeholder:text-theme-accent/50">
+									<Input min="1" max={versesInChapter} type="number" bind:value={endVerseSearch} autocomplete="off" placeholder="Nomor {term('verse')}" size="md" class="bg-transparent rounded-3xl px-4 w-32 placeholder:text-theme-accent/50">
 										<Search slot="left" size={6} classes="pt-1 {endVerseSearch.length > 0 && 'hidden'}" />
 									</Input>
 								</div>
 
-								<!-- Scrollable List -->
 								<div class="max-h-52 overflow-y-auto my-2 px-2">
 									{#each Array.from({ length: quranMetaData[$__chapterNumber].verses - $__audioSettings.startVerse + 1 }, (_, i) => i + $__audioSettings.startVerse).filter((v) => v.toString().includes(endVerseSearch)) as verse}
 										<DropdownItem
@@ -335,8 +255,7 @@
 												endVerseDropdownOpen = !endVerseDropdownOpen;
 											}}
 										>
-											{term('verse')}
-											{verse}
+											{term('verse')} {verse}
 										</DropdownItem>
 									{/each}
 								</div>
@@ -350,13 +269,10 @@
 		{#if $__audioSettings.audioType === 'verse'}
 			<div class="flex flex-col space-y-4 py-4 border-t border-theme-accent/20">
 				<div class="flex flex-row space-x-4">
-					<!-- repeat times -->
 					<div class="flex flex-row space-x-2">
-						<span class="m-auto text-sm"> Repeat </span>
+						<span class="m-auto text-sm">Ulangi</span>
 
-						<button class="{buttonClasses} text-sm">
-							<div>{$__audioSettings.timesToRepeat} {$__audioSettings.timesToRepeat > 1 ? 'times' : 'time'}</div>
-						</button>
+						<button class="{buttonClasses} text-sm"><div>{$__audioSettings.timesToRepeat} kali</div></button>
 						<Dropdown bind:open={timesToRepeatDropdownOpen} class="max-h-52 overflow-y-auto my-2 px-2">
 							{#each selectableRepeatTimes as n}
 								<DropdownItem
@@ -366,20 +282,16 @@
 										timesToRepeatDropdownOpen = !timesToRepeatDropdownOpen;
 									}}
 								>
-									{n}
-									{n > 1 ? 'times' : 'time'}
+									{n} kali
 								</DropdownItem>
 							{/each}
 						</Dropdown>
 					</div>
 
-					<!-- repeat delay -->
 					<div class="flex flex-row space-x-2">
-						<span class="m-auto text-sm">Delay </span>
+						<span class="m-auto text-sm">Jeda</span>
 
-						<button class="{buttonClasses} text-sm">
-							<div>{selectableAudioDelays[$__audioSettings.audioDelay].name}</div>
-						</button>
+						<button class="{buttonClasses} text-sm"><div>{selectableAudioDelays[$__audioSettings.audioDelay].name}</div></button>
 						<Dropdown bind:open={audioDelayDropdownOpen} class="max-h-52 overflow-y-auto my-2 px-2">
 							{#each Object.values(selectableAudioDelays) as delay}
 								<DropdownItem
@@ -387,8 +299,10 @@
 									on:click={() => {
 										$__audioSettings.audioDelay = delay.id;
 										audioDelayDropdownOpen = !audioDelayDropdownOpen;
-									}}>{delay.name}</DropdownItem
+									}}
 								>
+									{delay.name}
+								</DropdownItem>
 							{/each}
 						</Dropdown>
 					</div>
@@ -397,11 +311,11 @@
 		{/if}
 
 		<Checkbox checked={$__audioSettings.rememberSettings} on:click={() => toggleRememberSettings()} class="space-x-2 pb-6 font-normal bg-theme-bg">
-			<span>Remember Settings</span>
+			<span>Ingat Pengaturan</span>
 		</Checkbox>
 	</div>
 
 	<div class="flex-shrink-0 mt-4">
-		<button on:click={() => playButtonHandler($__audioSettings.playingKey)} class="w-full {buttonClasses} {invalidStartVerse || invalidEndVerse || invalidTimesToRepeat ? disabledClasses : null}">Play</button>
+		<button on:click={() => playButtonHandler($__audioSettings.playingKey)} class="w-full {buttonClasses} {invalidStartVerse || invalidEndVerse || invalidTimesToRepeat ? disabledClasses : null}">Putar</button>
 	</div>
 </Modal>
