991e3f28a831f1227ee78a720b98e4e6e79505d8
Ari Sulistiono <ari.sulistiono@gmail.com>
2026-09-21 10:14:39 +0700

    chore(r0): normalize modal backdrop closing tag

--- src/components/ui/FlowbiteSvelte/modal/Modal.svelte
+++ src/components/ui/FlowbiteSvelte/modal/Modal.svelte
@@ -116,7 +116,7 @@
 </script>
 
 {#if open}
-	<div class={backdropCls} />
+	<div class={backdropCls}></div>
 	<!-- svelte-ignore a11y-no-noninteractive-element-interactions -->
 	<div transition:fly={transitionParams} on:keydown={handleKeys} on:wheel|preventDefault|nonpassive use:prepareFocus use:focusTrap on:click={onAutoClose} on:mousedown={onOutsideClose} class={dialogCls} tabindex="-1" aria-modal="true" role="dialog">
 		<div class="{modalPositions[position].innerClasses} {sizes[size]} {modalPositions[position].sizes}">
